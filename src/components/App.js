00f46b8f3a96e606fc83896569dc3aea06b67974
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-01-19 20:58:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ const App = () => {
       <div id="expense-list">
       </div>
       <div id="total-expense">
-        'Total Expense: {expense}'
+        Total Expense: 400
       </div>
     </div>
   )
