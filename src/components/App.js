b6fad3af61b8cb9a67031a3127d9d247c6987117
Md Rizwan Ansari <85256728+mdrizwan7739@users.noreply.github.com>
2023-01-19 10:49:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React,{useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
+  const[expense,setExpense]=useState(0);
+  const handle =()=>{
+    const a =document.createElement("li");
+    const b=getElementById("expense-input";
+    a.innerHTML= document.)=b.value;
+    document.getElementById("expense-list").appendChild(a);
+    const c=b.indexOf("-");
+    const d =c.split(c);
+    setExpense(parseInt(d)+expense);
+  }
 
   return (
     <div id="main">
       <input id="expense-input" />
-      <button id="expense-button">Add Expense</button>
+      <button id="expense-button" onClick={handle}>Add Expense</button>
       <div id="expense-list">
       </div>
       <div id="total-expense">
-        Total Expense: 0
+        Total Expense: {expense};
       </div>
     </div>
   )
